c1516bd835a500e53fa43f7454756b1e295dc20f
Siwaÿll <siwayll@doublepsi.fr>
2025-02-07 06:55:28 +0100

    🔥🔥🔥 Nouveau départ 🔥🔥🔥
    Signed-off-by: Siwaÿll <siwayll@doublepsi.fr>

---
+++ dot_config/nvim/lua/plugins/test.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+      require("supermaven-nvim").setup({})
+    end,
+    event = "VeryLazy",
+  },
+  {
+    "folke/zen-mode.nvim",
+    cmd = { "ZenMode" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    dependencies = {
+      {
+        "folke/twilight.nvim",
+        config = function()
+          require("twilight").setup({
+            dim_inactive = true,
+          })
+        end,
+      },
+    },
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+}
