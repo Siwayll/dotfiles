eeaa3185dd0c0469f85ec06ee7340e85aba1f4f6
Siwaÿll <siwayll@doublepsi.fr>
2025-03-12 21:01:25 +0100

    update nvim

--- dot_config/nvim/lua/plugins/test.lua
+++ dot_config/nvim/lua/plugins/test.lua
@@ -27,7 +27,7 @@ return {
   },
   {
     "mrcjkb/rustaceanvim",
-    version = "^5", -- Recommended
-    lazy = false, -- This plugin is already lazy
+    version = "^5",
+    lazy = false,
   },
 }
